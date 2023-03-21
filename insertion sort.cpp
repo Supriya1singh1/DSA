14d2bd89570e52ce74cdb2a752a972bc11f16d5f
Supriya1singh1 <128466373+Supriya1singh1@users.noreply.github.com>
2023-03-21 17:03:46 +0530

    Create insertion sort.cpp

---
+++ insertion sort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    vector<int> arr = { 5, 2, 4, 6, 1, 3 };
+    insertionSort(arr);
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
