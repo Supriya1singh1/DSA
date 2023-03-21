59ec01a4cb8e18952cce0ca9a32b49ff68f9e04d
Supriya1singh1 <128466373+Supriya1singh1@users.noreply.github.com>
2023-03-21 16:45:23 +0530

    Add files via upload

---
+++ binars.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int low, int high, int key) {
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    // element not found
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 10;
+
+    // find the index of the key in the array
+    int index = binarySearch(arr, 0, n - 1, key);
+
+    if (index != -1) {
+        cout << "Element found at index " << index << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
